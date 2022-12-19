b62924088264716e6dbf50ed1300d45df709ab5d
nika7724 <nika0147@stud.kea.dk>
2022-12-19 10:05:51 +0100

    Initial commit

---
+++ src/main/java/com/example/examprojectfall2022/ExamProjectFall2022Application.java
@@ -0,0 +1,13 @@
+package com.example.examprojectfall2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamProjectFall2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamProjectFall2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examprojectfall2022/ExamProjectFall2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examprojectfall2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamProjectFall2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
